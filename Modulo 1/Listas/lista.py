fb0b4c381fc009a1ed2c79bd10bff2a64d2a8091
Maxstendey <cauaoliv.costa@gmail.com>
2026-05-12 08:40:05 -0300

    Add files via upload

---
+++ Modulo 1/Listas/lista.py
@@ -0,0 +1,11 @@
+nomes = ["Leclerc", "Hamilton", "Antonelli", "Bortoleto"]
+
+nomes.append("Russell")
+
+print("-" * 50)
+print(nomes[0])
+print(nomes[1])
+print(nomes[2])
+print(nomes[3])
+print(nomes[4])
+print("-" * 50)
